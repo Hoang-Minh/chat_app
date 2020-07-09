bc09301c7beecdc967b70fb5b6f67b7d1a86d9a4
Hoang-Minh <quataoxanh83@gmail.com>
2020-07-09 00:52:33 -0700

    refactored LoginPage

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -76,107 +76,118 @@ function LoginPage(props) {
     setSubmitting(false);
   };
 
+  const renderForm = (formik) => {
+    const {
+      handleSubmit,
+      values,
+      handleChange,
+      handleBlur,
+      errors,
+      touched,
+      handleRememberMe,
+      isSubmitting,
+    } = formik;
+
+    return (
+      <div className="app">
+        <Title level={2}>Log In</Title>
+        <form onSubmit={handleSubmit} style={{ width: "350px" }}>
+          <Form.Item required>
+            <Input
+              id="email"
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Enter your email"
+              type="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={
+                errors.email && touched.email
+                  ? "text-input error"
+                  : "text-input"
+              }
+            />
+            {errors.email && touched.email && (
+              <div className="input-feedback">{errors.email}</div>
+            )}
+          </Form.Item>
+
+          <Form.Item required>
+            <Input
+              id="password"
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Enter your password"
+              type="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={
+                errors.password && touched.password
+                  ? "text-input error"
+                  : "text-input"
+              }
+            />
+            {errors.password && touched.password && (
+              <div className="input-feedback">{errors.password}</div>
+            )}
+          </Form.Item>
+          {formErrorMessage && (
+            <label>
+              <p
+                style={{
+                  color: "#ff0000bf",
+                  fontSize: "0.7rem",
+                  border: "1px solid",
+                  padding: "1rem",
+                  borderRadius: "10px",
+                }}
+              >
+                {formErrorMessage}
+              </p>
+            </label>
+          )}
+
+          <Form.Item>
+            <Checkbox
+              id="rememberMe"
+              onChange={handleRememberMe}
+              checked={rememberMe}
+            >
+              Remember me
+            </Checkbox>
+            <a
+              className="login-form-forgot"
+              href="/reset_user"
+              style={{ float: "right" }}
+            >
+              forgot password
+            </a>
+            <div>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+                style={{ minWidth: "100%" }}
+                disabled={isSubmitting}
+                onSubmit={handleSubmit}
+              >
+                Log in
+              </Button>
+            </div>
+            Or <a href="/register">register now!</a>
+          </Form.Item>
+        </form>
+      </div>
+    );
+  };
+
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
-      {(formik) => (
-        <div className="app">
-          <Title level={2}>Log In</Title>
-          <form onSubmit={formik.handleSubmit} style={{ width: "350px" }}>
-            <Form.Item required>
-              <Input
-                id="email"
-                prefix={
-                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                placeholder="Enter your email"
-                type="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                className={
-                  formik.errors.email && formik.touched.email
-                    ? "text-input error"
-                    : "text-input"
-                }
-              />
-              {formik.errors.email && formik.touched.email && (
-                <div className="input-feedback">{formik.errors.email}</div>
-              )}
-            </Form.Item>
-
-            <Form.Item required>
-              <Input
-                id="password"
-                prefix={
-                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                placeholder="Enter your password"
-                type="password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                className={
-                  formik.errors.password && formik.touched.password
-                    ? "text-input error"
-                    : "text-input"
-                }
-              />
-              {formik.errors.password && formik.touched.password && (
-                <div className="input-feedback">{formik.errors.password}</div>
-              )}
-            </Form.Item>
-            {formErrorMessage && (
-              <label>
-                <p
-                  style={{
-                    color: "#ff0000bf",
-                    fontSize: "0.7rem",
-                    border: "1px solid",
-                    padding: "1rem",
-                    borderRadius: "10px",
-                  }}
-                >
-                  {formErrorMessage}
-                </p>
-              </label>
-            )}
-
-            <Form.Item>
-              <Checkbox
-                id="rememberMe"
-                onChange={formik.handleRememberMe}
-                checked={rememberMe}
-              >
-                Remember me
-              </Checkbox>
-              <a
-                className="login-form-forgot"
-                href="/reset_user"
-                style={{ float: "right" }}
-              >
-                forgot password
-              </a>
-              <div>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  className="login-form-button"
-                  style={{ minWidth: "100%" }}
-                  disabled={formik.isSubmitting}
-                  onSubmit={formik.handleSubmit}
-                >
-                  Log in
-                </Button>
-              </div>
-              Or <a href="/register">register now!</a>
-            </Form.Item>
-          </form>
-        </div>
-      )}
+      {(formik) => renderForm(formik)}
     </Formik>
   );
 }
