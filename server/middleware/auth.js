5e9dbcd894cbe17816dcb86bb5fa503ceafd0014
Hoang-Minh <quataoxanh83@gmail.com>
2020-07-08 20:53:58 -0700

    using async/await in auth() inside user_actions

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,14 +1,24 @@
-const { User } = require('../models/User');
+const { User } = require("../models/User");
 
-let auth = (req, res, next) => {
+let auth = async (req, res, next) => {
   let token = req.cookies.w_auth;
+
+  // const user = await User.findByToken(token);
+
+  // if (!user) {
+  //   return res.send({ isAuth: false, error: true });
+  // }
+
+  // req.token = token;
+  // req.user = user;
+  // next();
 
   User.findByToken(token, (err, user) => {
     if (err) throw err;
     if (!user)
       return res.json({
         isAuth: false,
-        error: true
+        error: true,
       });
 
     req.token = token;
